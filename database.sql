873d47e07d823af432a08da6e00630c849681f3b
N1ktt <76476591+N1ktt@users.noreply.github.com>
2022-10-04 12:20:16 +0200

    upadate database

--- database.sql
+++ database.sql
@@ -8,7 +8,19 @@ CREATE TABLE Users(
     passwordHash BINARY(32) NOT NULL
 );
 
-CREATE TABLE Tokens{
+CREATE TABLE Tokens(
     idToken INT NOT NULL AUTO_INRCEMENT PRIMARY KEY,
-    tokenValue INT NO NULL,
-};
+    tokenValue INT NOT NULL,
+    idUser INT NOT NULL,
+    expirationDate DATE NOT NULL,
+    expired BOOLEAN NOT NULL,
+    FOREIGN KEY (idUser) REFERENCES Users(idUser)
+);
+
+CREATE TABLE Days(
+    idDay INT NOT NULL AUTO_INRCEMENT PRIMARY KEY,
+    idUser INT NOT NULL,
+    dayDate DATE NOT NULL,
+    title STR NOT NULL,
+
+);
