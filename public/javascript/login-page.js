08da88f496f31250946e33a230ed02053da7eef0
N1ktt <76476591+N1ktt@users.noreply.github.com>
2022-10-21 20:41:55 +0200

    login and register

---
+++ public/javascript/login-page.js
@@ -0,0 +1,16 @@
+
+document.querySelector("#register").addEventListener("click",()=>{
+    console.log(document.querySelector(".triangle-log div"))
+    document.querySelector(".triangle div").classList.remove("on-login-traingle-flip")
+    document.querySelector(".triangle div").classList.add("on-register-traingle-flip")
+    document.querySelector("#login").classList.remove("choosen")
+    document.querySelector("#register").classList.add("choosen")
+})
+
+document.querySelector("#login").addEventListener("click",()=>{
+    console.log(document.querySelector(".triangle-log div"))
+    document.querySelector(".triangle div").classList.remove("on-register-traingle-flip")
+    document.querySelector(".triangle div").classList.add("on-login-traingle-flip")
+    document.querySelector("#register").classList.remove("choosen")
+    document.querySelector("#login").classList.add("choosen")
+})
