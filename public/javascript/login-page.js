8d6efed8b522cf8c7b469ae585dc2ecc096b7650
N1ktt <76476591+N1ktt@users.noreply.github.com>
2022-11-07 13:21:42 +0100

    Add files via upload

--- public/javascript/login-page.js
+++ public/javascript/login-page.js
@@ -1,20 +1,38 @@
-
-document.querySelector("#register").addEventListener("click",()=>{
-    console.log(document.querySelector(".triangle-log div"))
-    document.querySelector(".triangle div").classList.remove("on-login-traingle-flip")
-    document.querySelector(".triangle div").classList.add("on-register-traingle-flip")
-    document.querySelector("#login").classList.remove("choosen")
-    document.querySelector("#login").classList.add("not-choosen")
-    document.querySelector("#register").classList.add("choosen")
-    document.querySelector("#register").classList.remove("not-choosen")
-})
-
-document.querySelector("#login").addEventListener("click",()=>{
-    console.log(document.querySelector(".triangle-log div"))
+function turnOnLogin(){
+    // console.log(document.querySelector(".triangle-log div"))
+    document.querySelectorAll(".not-choosen").forEach(element => {
+        element.removeEventListener("click",turnOnLogin)
+    })
     document.querySelector(".triangle div").classList.remove("on-register-traingle-flip")
     document.querySelector(".triangle div").classList.add("on-login-traingle-flip")
     document.querySelector("#register").classList.remove("choosen")
     document.querySelector("#register").classList.add("not-choosen")
     document.querySelector("#login").classList.add("choosen")
     document.querySelector("#login").classList.remove("not-choosen")
-})
+
+    document.querySelectorAll(".not-choosen").forEach(element => {
+        element.addEventListener("click",turnOnRegister)
+    })
+    document.querySelector(".reg").style.display="none"
+    document.querySelector(".log").style.display="block"
+}
+
+function turnOnRegister(){
+    // console.log(document.querySelector(".triangle-log div"))
+    document.querySelectorAll(".not-choosen").forEach(element => {
+        element.removeEventListener("click",turnOnRegister)
+    })
+    document.querySelector(".triangle div").classList.remove("on-login-traingle-flip")
+    document.querySelector(".triangle div").classList.add("on-register-traingle-flip")
+    document.querySelector("#login").classList.remove("choosen")
+    document.querySelector("#login").classList.add("not-choosen")
+    document.querySelector("#register").classList.add("choosen")
+    document.querySelector("#register").classList.remove("not-choosen")
+
+    document.querySelectorAll(".not-choosen").forEach(element => {
+        element.addEventListener("click",turnOnLogin)
+    })
+    document.querySelector(".log").style.display="none"
+    document.querySelector(".reg").style.display="block"
+}
+turnOnLogin()
